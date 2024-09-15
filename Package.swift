9b33752442811ce0324d5641985e13c19a22953f
Phillip Avelar <36336702+pavelar@users.noreply.github.com>
2024-09-15 11:22:58 -0500

    Updates for IOS 17

--- Package.swift
+++ Package.swift
@@ -1,8 +1,22 @@
-//
-//  Package.swift
-//  
-//
-//  Created by Phillip Avelar on 9/15/24.
-//
+// swift-tools-version:5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 
-import Foundation
+import PackageDescription
+
+let package = Package(
+    name: "SpreadsheetView",
+    platforms: [
+        .iOS(.v16),
+    ],
+    products: [
+        .library(
+            name: "SpreadsheetView",
+            targets: ["SpreadsheetView"]),
+    ],
+    targets: [
+        .target(
+            name: "SpreadsheetView",
+            path: "Framework/Sources"
+        )
+    ]
+)
